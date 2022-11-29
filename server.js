443c9dbf84b13216cf30c84f4ebf93197a8a608f
Cyber <cybergen49@gmail.com>
2022-11-29 05:40:02 -0600

    Add some API methods

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const logger = require('cyber-express-logger');
 const utils = require('web-resources');
 const sqlite3 = require('better-sqlite3');
@@ -8,13 +9,35 @@ const srv = express();
 srv.set('view engine', 'ejs');
 srv.use(logger({ getIP: req => req.headers['cf-connecting-ip'] }));
 srv.use(express.static(`${__dirname}/web`, { index: false }));
+srv.use(bodyParser.json({ type: 'application/json' }));
 
 srv.use('/api', (req, res, next) => {
     req.db = sqlite3('main.db');
+    res.out = {};
+    req.validate = (name, value, condition) => {
+        if (value && condition) {
+            return true;
+        }
+        req.endError(`badRequest`, `The ${name} parameter is invalid.`, 401);
+        return false;
+    };
+    res.endSuccess = (code = 200) => {
+        res.out.success = true;
+        res.status(code).json(res.out);
+    };
+    res.endError = (short, long, code) => {
+        res.out.success = false;
+        res.error = { short: short, long: long };
+        res.status(code).json(res.out);
+    };
     res.on('finish', () => {
         db.close();
     });
     next();
+});
+
+srv.post('/api/login', (req, res) => {
+    // ...
 });
 
 srv.get('/*', (req, res, next) => {
